52557fc5cd59ccd9f1a4d8523caac655c94e9988
Tony Du <tony56a@gmail.com>
2014-07-12 20:26:27 -0400

    Device discovery dialog

--- NotifyrDemoApp/app/src/main/java/com/andrewhassan/notifyrdemoapp/MainActivity.java
+++ NotifyrDemoApp/app/src/main/java/com/andrewhassan/notifyrdemoapp/MainActivity.java
@@ -1,62 +1,97 @@
 package com.andrewhassan.notifyrdemoapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
 
+
+    private ArrayList<BluetoothDevice> m_devices;
+    private DeviceAdapter m_names;
+
+    private ProgressDialog barProgressDialog;
+
     private BluetoothAdapter m_bt_adapter;
+    private Handler m_handler;
+    private boolean m_running;
+
+    private static final String TAG = "NOTIFYR-APP";
+    private static final long SCAN_PERIOD = 3000;
 
     public void sendData(View view) {
-        EditText text_area = (EditText)this.findViewById(R.id.editText);
+        EditText text_area = (EditText) this.findViewById(R.id.editText);
         String str = text_area.getText().toString();
-        int length = str.length()/20;
-
+        int length = str.length() / 20;
 
         // Clear text area
         text_area.setText("");
 
         //must split string up into 20 byte chunks
-        for(int i=0; i<length; i++){
-            Log.d("MainActivity", "sendData...going to send message string \"" + str.substring(i*20,(i+1)*20) + "\"");
+        for (int i = 0; i < length; i++) {
+            Log.d("MainActivity", "sendData...going to send message string \"" + str.substring(i * 20, (i + 1) * 20) + "\"");
         }
 
-        Log.d("MainActivity", "sendData...going to send message string \"" + str.substring(length*20) + "\"");
+        Log.d("MainActivity", "sendData...going to send message string \"" + str.substring(length * 20) + "\"");
         // Initialize BT connection
         // Send str using BT interface
     }
 
-    public void sendTime(View v){
+    public void sendTime(View v) {
         //send time as a string, must be hours,minutes, and seconds. All values must be plus one, since we can have 0 values
         //(CANNOT HAVE!!!)
         Calendar c = Calendar.getInstance();
         int seconds = c.get(Calendar.SECOND);
         int minutes = c.get(Calendar.MINUTE);
         int hours = c.get(Calendar.HOUR_OF_DAY);
-        byte[] valuesToSend = new byte[]{(byte) hours+1, (byte) minutes+1, (byte)(seconds+1) };
-        Log.d("MainActivity", "sendData...going to send date string \"" +valuesToSend[0]+valuesToSend[1]+valuesToSend[2] + "\"");
+        byte[] valuesToSend = new byte[]{(byte) (hours + 1), (byte) (minutes + 1), (byte) (seconds + 1)};
+        Log.d("MainActivity", "sendData...going to send date string \"" + valuesToSend[0] + valuesToSend[1] + valuesToSend[2] + "\"");
 
+    }
+
+    public void showResults() {
+        if (m_names == null || m_names.isEmpty()) {
+            return;
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("aaa").setAdapter(m_names, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.create().show();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         BluetoothManager bt_manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
         m_bt_adapter = bt_manager.getAdapter();
+        m_handler = new Handler();
     }
 
 
@@ -69,7 +104,24 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.find_devices:
+                scanLeDevice(true);
+                return true;
+            case R.id.forget_device:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanLeDevice(false);
     }
 
     @Override
@@ -83,4 +135,74 @@ public class MainActivity extends Activity {
             if (m_bt_adapter == null) finish();
         }
     }
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            m_names = new DeviceAdapter(MainActivity.this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+            m_devices = new ArrayList<BluetoothDevice>();
+
+            barProgressDialog = new ProgressDialog(MainActivity.this);
+            barProgressDialog.setTitle("Searching for devices");
+            barProgressDialog.setMessage("Device search in progress...");
+            barProgressDialog.setProgressStyle(barProgressDialog.STYLE_SPINNER);
+            barProgressDialog.setCancelable(false);
+            barProgressDialog.show();
+
+            // Stops scanning after a pre-defined scan period.
+            m_handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    m_running = false;
+                    m_bt_adapter.stopLeScan(mLeScanCallback);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (barProgressDialog != null) {
+                                barProgressDialog.dismiss();
+                            }
+                            showResults();
+                        }
+                    });
+                }
+            }, SCAN_PERIOD);
+
+            m_running = true;
+            m_bt_adapter.startLeScan(mLeScanCallback);
+        } else {
+            if (barProgressDialog != null) {
+                barProgressDialog.dismiss();
+            }
+            m_running = false;
+            m_bt_adapter.stopLeScan(mLeScanCallback);
+        }
+    }
+
+    // Device scan callback.
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+                    Log.i(TAG, "Added device:" + device.getName());
+                    m_devices.add(device);
+                    m_names.add(device.getName());
+                }
+            };
+
+    private class DeviceAdapter extends ArrayAdapter<String> {
+
+        public DeviceAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+
+    }
+
+
 }
